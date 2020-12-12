8552367a243c376a2ae5174c5f43f720f3c1fc96
yessGlory <ozgurkurucan337@gmail.com>
2020-12-12 13:59:30 +0300

    v1.0.1

---
+++ Assets/Scripts/TagsVisible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TagsVisible : MonoBehaviour
+{
+    public List<TagsAR> etiketler = new List<TagsAR>();
+    
+    public bool allTagsİsVisible = false;
+
+    //Burada bütün etiketler gösterilip gizlenir.
+    public void TumEtiketleriGizle()
+    {
+        for (int i = 0; i < etiketler.Count; i++)
+        {
+            etiketler[i].Goster();
+        }
+    }
+}
---
+++ Assets/Scripts/TurnModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnModel : MonoBehaviour
+{
+    bool isTurnActive = false;
+    GameObject ActiveModel;
+    
+    public void ActivatedTurnModel()
+    {
+        //Aktif Modeli bul.
+        //Değişkeni true yap.
+        ActiveModel = GameObject.FindGameObjectWithTag("ActiveModel");
+        isTurnActive = !isTurnActive;
+    }
+
+    public void TurnActiveModel()
+    {
+        //Döndürme Fonksiyonu
+        Vector3 rotateVectors = new Vector3(0,0.5f,0);
+        ActiveModel.transform.Rotate(rotateVectors);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isTurnActive)
+        {
+            TurnActiveModel();
+        }
+    }
+   
+}

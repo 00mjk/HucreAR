5cd9224a64add869459d92fee4d3f5a2adf9d9e2
yessGlory <ozgurkurucan337@gmail.com>
2020-12-13 23:09:14 +0300

    v1.0.1
    -Outline Shader for selected organel.
    -Organel Get Info
    -Organel Find Info

---
+++ Assets/Scripts/Organel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Organel
+{
+    public string Etiket;
+    public GameObject OrganelModel;
+    public string OrganelBilgi;
+    public Material DefaultMaterial;
+    //Alt Bolumleri Al
+    public List<OrganelAltBolum> AltBolumler = new List<OrganelAltBolum>();
+
+    //Organel Bilgi Canvası eklenecek.
+
+    bool isOutline = false;
+
+    //Kendini ve alt bolumlerini outline ekleyecek. 
+    public void SetOutline(Shader Outline)
+    {
+        if (!isOutline)
+        {
+            
+            OrganelModel.GetComponent<Renderer>().material.shader = Outline;
+            isOutline = true;
+        }
+        else
+        {
+            OrganelModel.GetComponent<Renderer>().material = DefaultMaterial;
+            isOutline = false;
+        }
+            
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TouchController : MonoBehaviour
+{
+    [Header("Organel Manager")]
+    public OrganelManager organelManager;
+    [Header("Bilgi Texti")]
+    public TextMeshProUGUI InfoText;
+
+    [Header("Outline Shader")]
+    public Shader outline;
+
+   
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Touch();
+        }
+    }
+
+
+    //Dokunmayı kontrol eden fonksiyon
+     void Touch()
+     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        //Debug.Log("Okçu Aktiflik: " + isArcherActive);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.LogError("Touch : " + hit.collider.tag);
+
+            GetOrganelInfo(InfoText, hit.collider.tag);
+        }
+     } 
+
+
+    void GetOrganelInfo(TextMeshProUGUI InfoText, string OrganelTag)
+    {
+        InfoText.text = SearchOrganelForInfo(OrganelTag);
+    }
+
+    //Organelde ara
+
+    string SearchOrganelForInfo(string organelTag)
+    {
+        string wasFoundInfo = "";
+        foreach (var item in organelManager.Organeller)
+        {
+            
+            if(item.Etiket == organelTag)
+            {
+                wasFoundInfo = item.OrganelBilgi;
+                item.SetOutline(outline);
+                break;
+            }
+        }
+
+        return wasFoundInfo;
+    }
+}

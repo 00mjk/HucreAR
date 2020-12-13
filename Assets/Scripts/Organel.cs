79c14ca8a4d48a317e5533cc2bc76b9604ae6fe8
yessGlory <ozgurkurucan337@gmail.com>
2020-12-13 17:18:18 +0300

    v1.0.1

---
+++ Assets/Scripts/Organel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Organel
+{
+    public string Etiket;
+    public GameObject OrganelModel;
+    public string OrganelBilgi;
+
+    //Alt Bolumleri Al
+    public List<OrganelAltBolum> AltBolumler = new List<OrganelAltBolum>();
+
+    //Organel Bilgi Canvası eklenecek.
+
+
+
+
+    //Kendini ve alt bolumlerini outline ekleyecek. 
+    public void SetOutline()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TouchController : MonoBehaviour
+{
+    public OrganelManager organelManager;
+    
+    public TextMeshProUGUI InfoText;
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Touch();
+        }
+    }
+
+
+    //Dokunmayı kontrol eden fonksiyon
+     void Touch()
+     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        //Debug.Log("Okçu Aktiflik: " + isArcherActive);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.LogError("Touch : " + hit.collider.tag);
+
+            GetOrganelInfo(InfoText, hit.collider.tag);
+        }
+     } 
+
+
+    void GetOrganelInfo(TextMeshProUGUI InfoText, string OrganelTag)
+    {
+        InfoText.text = SearchOrganelForInfo(OrganelTag);
+    }
+
+    //Organelde ara
+
+    string SearchOrganelForInfo(string organelTag)
+    {
+        string wasFoundInfo = "";
+        foreach (var item in organelManager.Organeller)
+        {
+            
+            if(item.Etiket == organelTag)
+            {
+                wasFoundInfo = item.OrganelBilgi;
+                break;
+            }
+        }
+
+        return wasFoundInfo;
+    }
+}

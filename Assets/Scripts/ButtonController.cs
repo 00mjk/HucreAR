ac8114946fc3ac6fdfe264dad721ee2bfb222ec3
yessGlory <ozgurkurucan337@gmail.com>
2021-01-07 10:48:58 +0300

    v 1.0.3

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject InfoPanel;
+    public AudioSource BlobTouchEffect;
+
+    public TouchController TouchController;
+    
+    public GameObject İleriButton;
+    public GameObject GeriButton;
+
+    int index = 0;
+
+    public OrganelManager organelMan;
+    
+    public void ClosePanel()
+    {
+        //InfoPanel.SetActive(false);
+
+        //Burada Close Animation Calıştırılacak
+
+        Animator animator = InfoPanel.GetComponent<Animator>();
+        bool isOpen = animator.GetBool("isOpen");
+        if(animator != null)
+        {
+            animator.SetBool("isOpen",false);
+            BlobTouchEffect.Play();
+            TouchController.isActive = false;
+            Debug.LogError("Basıldı Animator Var!");
+        }
+
+        Debug.LogError("Basıldı Animator Yok!");
+
+        //Kapatılıncı Hepsinin Outlinenını Sil
+
+        foreach (var item in organelMan.Organeller)
+        {
+            item.RemoveOutline();
+        }
+
+    }
+
+    public void ForwardInfo()
+    {
+        
+        Organel organel = TouchController.GetFindedOrganel();
+
+        if (index < organel.OrganelBilgi.Count - 1)
+        {
+            
+            index++;
+
+            TouchController.InfoText.text = organel.OrganelBilgi[index];
+        }
+        else
+        {
+            İleriButton.SetActive(false);
+            GeriButton.SetActive(true);
+        }
+    }
+
+    public void BackInfo()
+    {
+        Organel organel = TouchController.GetFindedOrganel();
+
+        if(index > 0)
+        {
+            //Debug.LogError("BİLGİ SONUCU => " + organel.OrganelBilgi[index--]);
+
+            index--;
+
+            TouchController.InfoText.text = organel.OrganelBilgi[index];
+        }
+        else
+        {
+            GeriButton.SetActive(false);
+            İleriButton.SetActive(true);
+        }
+
+
+    }
+}

3b4cb3ebfdabf976490166d36501214a45a83345
yessGlory <ozgurkurucan337@gmail.com>
2021-01-16 23:32:44 +0300

    v 0.8

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject LoadCanvas;
+    public void LoadAnimalCell()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+    public void LoadAnimalCellButtonFunc()
+    {
+           StartCoroutine(loadAnimation());
+    }
+
+
+    public void LoadPlantCell()
+    {
+        SceneManager.LoadScene("PlantCell");
+    }
+
+
+    IEnumerator loadAnimation()
+    {
+        LoadCanvas.SetActive(true);
+        anim.SetBool("LoadSceneAnim",true);
+
+        yield return new WaitForSeconds(2f);
+
+        LoadAnimalCell();
+    }
+}
